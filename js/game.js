b9783aaf285b557e7bec50014d0735b3c18e1c1f
Mark Nachazel <mark.nachazel@altamiracorp.com>
2014-06-11 20:24:37 -0400

    finish initial setup of game

--- js/game.js
+++ js/game.js
@@ -2,10 +2,28 @@ function drawBoard() {
   var canvas = document.getElementById('game');
   if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
+    drawBlocks(canvas, ctx);
+    drawPaddle(canvas, ctx);
   }
-  var blockWidth = (canvas.width / 10.0) - 2.0;
-  for (var x = 0; x < canvas.width; x += blockWidth) {
-    ctx.fillStyle = "rgb(0,0,0)";
-    ctx.fillRect(x + 1, 0, blockWidth, 5);
+
+}
+function drawBlocks(canvas, ctx) {
+  var blockWidth = (canvas.width / 10.0);
+  var rowHeight = 10;
+  var numRows = 5;
+  for (var y = 0; y < numRows; y ++) {
+    for (var x = 0; x < canvas.width; x += blockWidth) {
+      ctx.fillStyle = "black";
+      ctx.fillRect(x + 1, y * (rowHeight+2), blockWidth - 2, rowHeight);
+    }
   }
+}
+
+function drawPaddle(canvas, ctx) {
+  ctx.fillStyle = "black";
+  var blockWidth = (canvas.width / 10.0);
+  var rowHeight = 10;
+  var x = (canvas.width - blockWidth) / 2;
+  var y = (canvas.height - rowHeight);
+  ctx.fillRect(x, y, blockWidth - 2, rowHeight - 2);
 }
