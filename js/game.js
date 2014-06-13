53c07b5977c70c1d3fdfe229446fe9438c527da6
Mark Nachazel <mark.nachazel@altamiracorp.com>
2014-06-12 20:12:18 -0400

    refactor code into beginning of draw loop

--- js/game.js
+++ js/game.js
@@ -1,41 +1,64 @@
-function drawBoard() {
+function onLoad() {
+  window.onkeyup = function(e) {
+    if (e.keyIdentifier == "Left") {
+      paddle['x'] -= 3;
+    }
+    else if (e.keyIdentifier == "Right") {
+      paddle['x'] += 3;    }
+  };
+
   var canvas = document.getElementById('game');
-  if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
-    drawBlocks(canvas, ctx);
-    drawPaddle(canvas, ctx);
-  }
-}
-function drawBlocks(canvas, ctx) {
-  var blockWidth = (canvas.width / 10.0);
-  var rowHeight = 10;
-  var numRows = 5;
-  for (var y = 0; y < numRows; y ++) {
-    for (var x = 0; x < canvas.width; x += blockWidth) {
-      ctx.fillStyle = "black";
-      ctx.fillRect(x + 1, y * (rowHeight+2), blockWidth - 2, rowHeight);
+  var FPS = 60;
+  var BLOCK_WIDTH = canvas.width / 10;
+  var BLOCK_HEIGHT = 10;
+  var BALL_RADIUS = 5;
+  var NUM_ROWS = canvas.width / BLOCK_WIDTH;
+  var ball = {
+    x: (canvas.width - BALL_RADIUS) / 2,
+    y: (canvas.height - BALL_RADIUS) / 2
+  };
+  var paddle = {
+    x: (canvas.width - BLOCK_WIDTH) / 2,
+    y: (canvas.height - BLOCK_HEIGHT)
+  };
+  var blocks = new Array(5);
+
+  function init() {
+    for (var y = 0; y < 5; y ++) {
+      blocks[y] = new Array(NUM_ROWS);
+      for (var x = 0; x < NUM_ROWS; x++) {
+        blocks[y][x] = { x: x * BLOCK_WIDTH + 1, y: y * (BLOCK_HEIGHT + 2)};
+      }
     }
   }
-}
 
-function drawPaddle(canvas, ctx) {
-  ctx.fillStyle = "black";
-  var blockWidth = (canvas.width / 10.0);
-  var rowHeight = 10;
-  var x = (canvas.width - blockWidth) / 2;
-  var y = (canvas.height - rowHeight);
-  ctx.fillRect(x, y, blockWidth - 2, rowHeight - 2);
-}
-
-function movePaddle(n) {
-  console.log(n);
-}
-
-window.onkeyup = function(e) {
-  if (e.keyIdentifier == "Left") {
-    movePaddle(-1);
+  function draw() {
+    if (canvas.getContext) {
+      var ctx = canvas.getContext('2d');
+      drawBlocks(canvas, ctx);
+      drawPaddle(canvas, ctx);
+//      drawBall();
+    }
   }
-  else if (e.keyIdentifier == "Right") {
-    movePaddle(1);
+  function drawBlocks(canvas, ctx) {
+    for (var y = 0; y < 5; y++) {
+      for (var x = 0; x < NUM_ROWS; x++) {
+        ctx.fillRect(blocks[y][x]['x'], blocks[y][x]['y'], BLOCK_WIDTH - 2, BLOCK_HEIGHT);
+      }
+    }
   }
-};
+
+  function drawPaddle(canvas, ctx) {
+    ctx.fillStyle = "black";
+    ctx.fillRect(paddle['x'], paddle['y'], BLOCK_WIDTH - 2, BLOCK_HEIGHT - 2);
+  }
+
+  init();
+  draw();
+
+}
+
+
+
+
+
